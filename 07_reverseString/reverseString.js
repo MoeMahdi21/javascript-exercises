80ba4ca0aaff84b0226d6018fed9e1b6211d8a0d
MohammedMahdi <99882304+MoeMahdi21@users.noreply.github.com>
2025-08-26 23:10:27 -0400

    complete exercise 07

--- 07_reverseString/reverseString.js
+++ 07_reverseString/reverseString.js
@@ -1,5 +1,14 @@
-const reverseString = function() {
+const reverseString = function (str) {
+  // let strArray = str.split('');
+  // let reverseString = [];
 
+  // for (let i = strArray.length - 1; i != -1; i--) {
+  //   reverseString.push(strArray[i]);
+  // }
+
+  // return reverseString.join('');
+
+  return str.split('').reverse().join('');
 };
 
 // Do not edit below this line
