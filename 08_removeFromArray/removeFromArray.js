c471c4ccacace1888cfc9f727c9505d184255b5a
MohammedMahdi <99882304+MoeMahdi21@users.noreply.github.com>
2025-08-27 22:16:02 -0400

    complete exercise 08

--- 08_removeFromArray/removeFromArray.js
+++ 08_removeFromArray/removeFromArray.js
@@ -1,4 +1,16 @@
-const removeFromArray = function() {
+const removeFromArray = function (arr, ...args) {
+  // let newArr = [];
+
+  // arr.forEach((item) => {
+  //   if (!arg.includes(item)) {
+  //     newArr.push(item);
+  //   }
+  // });
+  // return newArr;
+
+  return arr.filter((val) => {
+    return !args.includes(val);
+  });
 };
 
 // Do not edit below this line
