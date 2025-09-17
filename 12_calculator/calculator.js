918df78a158580b5df1b620061619eae454e89b1
MohammedMahdi <99882304+MoeMahdi21@users.noreply.github.com>
2025-09-16 20:04:05 -0400

    complete exercise 12

--- 12_calculator/calculator.js
+++ 12_calculator/calculator.js
@@ -1,25 +1,43 @@
-const add = function() {
-	
+const add = function (num1, num2) {
+  return num1 + num2;
 };
 
-const subtract = function() {
-	
+const subtract = function (num1, num2) {
+  return num1 - num2;
 };
 
-const sum = function() {
-	
+const sum = function (arr) {
+  if (arr.length === 0) {
+    return 0;
+  }
+
+  let sum = null;
+  for (num of arr) {
+    sum += num;
+  }
+
+  return sum;
 };
 
-const multiply = function() {
+const multiply = function (arr) {
+  let multiply = 1;
+  for (num of arr) {
+    multiply *= num;
+  }
 
+  return multiply;
 };
 
-const power = function() {
-	
+const power = function (num1, num2) {
+  return num1 ** num2;
 };
 
-const factorial = function() {
-	
+const factorial = function (num) {
+  let numFactorial = 1;
+  for (let i = num; i >= 1; i--) {
+    numFactorial *= i;
+  }
+  return numFactorial;
 };
 
 // Do not edit below this line
@@ -29,5 +47,5 @@ module.exports = {
   sum,
   multiply,
   power,
-  factorial
+  factorial,
 };
