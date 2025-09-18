50fd1a5b9830b5f841e4e2ab095ccd20ff9fb5d8
MohammedMahdi <99882304+MoeMahdi21@users.noreply.github.com>
2025-09-17 21:29:47 -0400

    complete exercise 13

--- 13_palindromes/palindromes.js
+++ 13_palindromes/palindromes.js
@@ -1,5 +1,8 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+  const newStr = str.replace(/[^\w]|_/g, '').toLowerCase();
+  console.log(newStr);
+  let strReverse = newStr.split('').reverse().join('');
+  return newStr === strReverse ? true : false;
 };
 
 // Do not edit below this line
